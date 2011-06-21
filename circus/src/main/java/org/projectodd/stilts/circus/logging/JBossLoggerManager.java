d54a6b00e98a73a975ca785b6ce929c800eab5a5
Bob McWhirter <bob@mcwhirter.org>
2011-06-21 17:30:51 -0400

    Re-mark everything as Apache License 2.0

--- circus/src/main/java/org/projectodd/stilts/circus/logging/JBossLoggerManager.java
+++ circus/src/main/java/org/projectodd/stilts/circus/logging/JBossLoggerManager.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2011 Red Hat, Inc, and individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the Apache License 2.0. 
+ * 
+ * You should have received a copy of the Apache License 
+ * along with this software; if not, please see:
+ * http://apache.org/licenses/LICENSE-2.0.txt
+ */
+
 package org.projectodd.stilts.circus.logging;
 
 import org.projectodd.stilts.logging.Logger;
--- circus/src/main/java/org/projectodd/stilts/circus/xa/psuedo/PsuedoXAResourceManager.java
+++ circus/src/main/java/org/projectodd/stilts/circus/xa/psuedo/PsuedoXAResourceManager.java
@@ -1,20 +1,12 @@
 /*
- * Copyright 2008-2011 Red Hat, Inc, and individual contributors.
+ * Copyright 2011 Red Hat, Inc, and individual contributors.
  * 
  * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * under the terms of the Apache License 2.0. 
  * 
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the Apache License 
+ * along with this software; if not, please see:
+ * http://apache.org/licenses/LICENSE-2.0.txt
  */
 
 package org.projectodd.stilts.circus.xa.psuedo;
