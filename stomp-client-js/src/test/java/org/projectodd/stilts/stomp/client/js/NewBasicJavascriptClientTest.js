7fcb9a54c1e3fff8cb19e72110321654eccb3d9b
Bob McWhirter <bob@mcwhirter.org>
2011-07-21 23:45:30 -0400

    Remove debug and explicit failure.

--- stomp-client-js/src/test/java/org/projectodd/stilts/stomp/client/js/NewBasicJavascriptClientTest.js
+++ stomp-client-js/src/test/java/org/projectodd/stilts/stomp/client/js/NewBasicJavascriptClientTest.js
@@ -1,16 +1,12 @@
 
 var Assert = org.junit.Assert;
 
-java.lang.System.err.println( "tests 1: " + tests.length );
 it( "should work", function() {
-  //Assert.fail( "damnit" );
+  // Assert.fail( "damnit" );
 } );
-
-java.lang.System.err.println( "tests 2: " + tests.length );
 
 it( "should also work", function() {
 
 } );
 
-java.lang.System.err.println( "tests 3: " + tests.length );
 
